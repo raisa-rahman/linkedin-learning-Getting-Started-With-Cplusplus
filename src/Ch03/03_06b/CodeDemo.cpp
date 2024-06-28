94e9000eb9320e7f175510f53ef9a64dba06bdcf
Raisa Rahman <124052639+raisa-rahman@users.noreply.github.com>
2024-06-28 11:31:01 +0000

    Pointers

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -6,6 +6,19 @@
 #include <string>
 
 int main(){
+    int a = 37;
+    int *ptr;
+
+    ptr = &a;
+
+    std::cout << "           The Content of a is " << a << std::endl;
+    std::cout << "ptr is pointing to the address " << ptr << std::endl;
+    std::cout << "           The address of a is " << &a << std::endl;
+    std::cout << "Where ptr is pointing, we have " << *ptr << std::endl;
+    std::cout << "         The address of ptr is " << &ptr << std::endl;
+
+
+
 
     std::cout << std::endl << std::endl;
     return (0);
